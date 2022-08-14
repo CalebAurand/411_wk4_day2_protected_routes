f0499aa8c40c398aac284dc9f665bf91393a37ee
CalebAurand <caurand13@gmail.com>
2022-08-13 21:31:13 -0500

    Updates

---
+++ precache-manifest.dd4f6bf5ef480662749591865644ed12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293065e791eb0780953c6a5f38bd5624",
+    "url": "/CalebAurand/411_wk4_day2_protected_routes/index.html"
+  },
+  {
+    "revision": "42312aad8778b9b7c93d",
+    "url": "/CalebAurand/411_wk4_day2_protected_routes/static/css/main.909db9c4.chunk.css"
+  },
+  {
+    "revision": "e6a816964a538126d80c",
+    "url": "/CalebAurand/411_wk4_day2_protected_routes/static/js/2.5ddee754.chunk.js"
+  },
+  {
+    "revision": "42312aad8778b9b7c93d",
+    "url": "/CalebAurand/411_wk4_day2_protected_routes/static/js/main.c255c587.chunk.js"
+  },
+  {
+    "revision": "bc625bdc477814ca49e8",
+    "url": "/CalebAurand/411_wk4_day2_protected_routes/static/js/runtime~main.3cf297c9.js"
+  }
+]);
