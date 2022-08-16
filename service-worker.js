7ebfdd0f93de8396161a4f8f92d7f3f6b7f9b2a0
CalebAurand <caurand13@gmail.com>
2022-08-16 18:43:12 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CalebAurand/411_wk4_day2_protected_routes/precache-manifest.dd4f6bf5ef480662749591865644ed12.js"
+  "/411_wk4_day2_protected_routes/precache-manifest.317ac1af78ac7b8790feb23a3fa61a87.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CalebAurand/411_wk4_day2_protected_routes/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/411_wk4_day2_protected_routes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
